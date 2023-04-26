49a772dedc620b81358ade378ae5c856e10520f1
Jakub Zajda <jakubzajda@gmail.com>
2023-04-26 18:56:30 +0200

    feat: Create RMService

--- RickAndMorty/Controllers/Core/RMCharactersViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharactersViewController.swift
@@ -13,27 +13,5 @@ final class RMCharactersViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
-        
-        let request = RMRequest(endpoint: .character,
-                                pathComponents: ["1"],
-                                queryParameters: [
-                                    URLQueryItem(name: "name", value: "rick"),
-                                    URLQueryItem(name: "status", value: "alive")
-                                ])
-        
-        print(request.url)
-        
-        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
-            switch result {
-            case .success(let character):
-                <#code#>
-            case .failure(let failure):
-                <#code#>
-            }
-        }
-
     }
-    
-
-
 }
---
+++ RickAndMorty/Models/APIResponseTypes/RMGetAllCharacterRespons.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetCharacterRespons.swift
+//  RickAndMorty
+//
+//  Created by Jakub Zajda on 26/04/2023.
+//
+
+import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next:  String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [RMCharacter]
+}
